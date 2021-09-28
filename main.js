91e2eb36ee6a692e0f23213eda2c1c3d2d1d8a42
scorN17 <scorn17@gmail.com>
2021-09-28 10:31:46 +0300

    tray

--- main.js
+++ main.js
@@ -4,14 +4,16 @@
 // For more info, see:
 // https://electronjs.org/docs/api/browser-window
 
-const { app, BrowserView, BrowserWindow, ipcMain } = require('electron')
+const { app, BrowserWindow, BrowserView, ipcMain, Menu, Tray, nativeImage } = require('electron')
 const path = require('path')
 
 app.whenReady().then(() => {
 
 	let win = new BrowserWindow({
+		// skipTaskbar: true,
 		autoHideMenuBar: true,
 		darkTheme: true,
+		icon: './tpl/images/trayicon.png',
 		webPreferences: {
 			preload: path.join(__dirname, 'preload.js'),
 			devTools: false,
@@ -23,34 +25,60 @@ app.whenReady().then(() => {
 
 	// win.webContents.openDevTools()
 
+	win.loadFile('index.html')
+
+	ipcMain.on('toMain', (event, data) => {
+		if (
+			'pnl_cntrpnl_size' == data.act
+			&& view
+			&& data.w
+			&& data.h
+		) {
+			data.w = Math.round(data.w);
+			data.h = Math.round(data.h);
+			console.log(view);
+			view.setBounds({ x: 200, y: 0, width: data.w, height: data.h })
+		}
+	})
+
 	const view = new BrowserView({
 		webPreferences: {
-			partition: 'persist:scorn3',
+			partition: 'persist:telega1',
 			sandbox: true,
 			devTools: false,
 		}
 	})
 	win.addBrowserView(view)
+	view.setBounds({ x: 200, y: 0, width: 300, height: 300 })
 	view.webContents.loadURL('https://web.telegram.org/z/',{
 		userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.82 Safari/537.36'
 	})
 
-	// const view = new BrowserView({
-	// 	webPreferences: {
-	// 		partition: 'persist:scorn3'
-	// 	}
-	// })
-	// win.addBrowserView(view)
-	// view.webContents.loadURL('https://web.telegram.org/z/',{
-	// 	userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.82 Safari/537.36'
-	// })
+	const template = [
+		{
+			label: 'Файл',
+			submenu: [
+				{
+					label: 'Закрыть',
+					role: 'quit'
+				}
+			]
+		},
+	]
+  
+	const menu = Menu.buildFromTemplate(template)
+	Menu.setApplicationMenu(menu)
 
-	ipcMain.on('toMain', (event, data) => {
-		console.log(data);
-		if ('pnl_cntrpnl_size' == data.act) {
-			view.setBounds({ x: 200, y: 0, width: data.w, height: data.h })
-		}
+	tray = new Tray('./tpl/images/trayicon.ico')
+  
+	const contextMenu = Menu.buildFromTemplate([
+		{ label: 'Закрыть', role: 'quit' },
+	  ])
+  
+	tray.setToolTip('Xuyanz')
+	tray.setContextMenu(contextMenu)
+
+	tray.on('double-click',()=>{
+		win.restore()
 	})
-
-	win.loadFile('index.html')
 })
